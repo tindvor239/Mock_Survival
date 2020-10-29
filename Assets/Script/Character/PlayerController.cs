7a625b173fc082216919a31c751a068e8c13bad4
tindvor239 <37416837+tindvor239@users.noreply.github.com>
2020-10-30 03:16:27 +0700

    move point

---
+++ Assets/Script/Character/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : CharacterController
+{
+    [SerializeField]
+    private GameObject target;
+    private NavMeshAgent agent;
+    protected override void Awake()
+    {
+        base.Awake();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        GetPositionOnClick(Input.GetMouseButtonDown(0));
+        MoveToPoint();
+    }
+    private void MoveToPoint()
+    {
+        if(target != null)
+        {
+            agent.Move(target.transform.position);
+        }
+    }
+    private void GetPositionOnClick(bool isInput)
+    {
+        if(isInput)
+        {
+            RaycastHit hit;
+            Ray ray = gameManager.MainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                target = CreateMovePoint(hit.point, "Point");
+            }
+        }
+    }
+    public GameObject CreateMovePoint (Vector3 position, string name)
+    {
+        GameObject newObject = new GameObject();
+        newObject.transform.position = position;
+        newObject.AddComponent(typeof(MoveTarget));
+        newObject.name = name;
+        return newObject;
+    }
+}
---
+++ Assets/Script/Particle/MoveTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    [SerializeField]
+    private float selfDestructTime;
+    private float currentDestructTime;
+    private void Awake()
+    {
+        currentDestructTime = selfDestructTime;
+    }
+    private void Update()
+    {
+        currentDestructTime -= Time.deltaTime;
+        if (currentDestructTime <= 0.0f)
+            SelfDestruct();
+    }
+    private void SelfDestruct()
+    {
+        DestroyImmediate(gameObject);
+    }
+}
